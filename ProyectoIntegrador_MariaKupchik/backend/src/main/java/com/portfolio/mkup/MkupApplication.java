b6887b75370c71b13cbab6fa4bd80b844a71e0cf
Mariakupchik <105744656+Mariakupchik@users.noreply.github.com>
2022-06-14 12:39:12 -0300

    CREE BACKEND

---
+++ ProyectoIntegrador_MariaKupchik/backend/src/main/java/com/portfolio/mkup/MkupApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.mkup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkupApplication.class, args);
+	}
+
+}
